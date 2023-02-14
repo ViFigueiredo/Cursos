92c021610a075ac9ee89ac63f76db8f5ecd2255c
Vinicius Figueiredo <vifigcode@gmail.com>
2023-02-14 18:00:57 -0300

    init -> pinia com option api

---
+++ CFS_Pinia/src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+import CounterComponents from "./components/CounterComponents.vue";
+import { useCounterStore } from "./stores/counter";
+const counter = useCounterStore();
+console.log(counter);
+</script>
+
+<template>
+  <div class="app">
+    <h1>Aprendendo Pinia com Context API</h1>
+    {{ counter.showCount }}
+    <button @click="counter.increment">Incrementar</button>
+
+    <br />
+    <br />
+    <br />
+    <hr />
+
+    <CounterComponents />
+  </div>
+</template>
+
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+</style>
---
+++ CFS_Pinia/src/components/CounterComponents.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { useCounterStore } from "../stores/counter";
+const counter = useCounterStore();
+</script>
+
+<template>
+  <div class="app">
+    <h1>Aprendendo Pinia com Context API</h1>
+    {{ counter.showCount }}
+    <button @click="counter.increment">Incrementar</button>
+  </div>
+</template>
