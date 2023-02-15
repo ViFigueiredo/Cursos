ef9e9ab6856b8b60de7b50c58f5d9a1e476a4e61
Vinicius Figueiredo <vifigcode@gmail.com>
2023-02-15 09:38:07 -0300

    exemplo componente em composition api

---
+++ CFS_Vue3_Composition_API/src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import {ref} from 'vue'
+const count = ref(0)
+
+function increment() {
+  count.value++
+}
+
+function decrement() {
+  count.value--
+}
+</script>
+
+<template>
+  <h2> APIs </h2>
+  {{ count }}
+  <button @click="increment"> Add </button>
+  <button @click="decrement"> Remove </button>
+</template>
+
+<style scoped>
+
+</style>
